002f9b85355675b627bdf534813e9946be594ccb
AEC-Dev97 <civileng.mahmoudelgohary@gmail.com>
2025-09-14 21:25:56 +0300

    Add files via upload

---
+++ Source/TwinPlusV2/TwinPlusV2.Build.cs
@@ -0,0 +1,23 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TwinPlusV2 : ModuleRules
+{
+	public TwinPlusV2(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Slate", "SlateCore", "ToolMenus", "EditorWidgets", "UnrealEd" });
+
+        PrivateIncludePaths.AddRange(new[] {""});
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
+}
